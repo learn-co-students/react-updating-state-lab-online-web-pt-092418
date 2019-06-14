06efe8678bbb295f86370f40c56320b1b16ad6e5
github username <github email address>
2019-06-13 21:30:05 -0400

    click incremental worked

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,27 @@
-// Code DigitalClicker Component Here
+import React from 'react';
+export default class DigitalClicker extends React.Component {
+
+  constructor() {
+    super()
+    this.state = {
+      timesClicked: 0
+    }
+  }
+
+  increment = () => {
+    let clickCount = this.state.timesClicked +1
+    this.setState({
+      timesClicked: clickCount
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.increment}>
+          {this.state.timesClicked}
+        </button>
+      </div>
+    )
+  }
+}
